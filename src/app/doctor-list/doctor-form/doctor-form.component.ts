03b5a7ccd24b73f97e7c0fb12548225f7157b6cf
Vasanth-Dev-Hub <vasanthk2599@gmail.com>
2025-05-08 13:38:24 +0530

    add,view,edit doctor page

--- src/app/doctor-list/doctor-form/doctor-form.component.ts
+++ src/app/doctor-list/doctor-form/doctor-form.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-doctor-form',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './doctor-form.component.html',
   styleUrl: './doctor-form.component.css'
 })
-export class DoctorFormComponent {
+export class DoctorFormComponent implements OnInit, OnChanges {
+  specializationList = ['Cardiologist', 'Cardiac Electrophysiologist', 'Neurologist', 'Neurosurgeon', 'Nuclear Medicine Specialist'];
+  doctorForm: FormGroup;
+  @Input() action: string = '';
+  @Input() data: any;
+  @Output() doctorFormSubmit = new EventEmitter<any>();
 
+  constructor(private fb: FormBuilder) {
+    this.doctorForm = this.fb.group({
+      name:[null,[Validators.required, Validators.minLength(3), Validators.pattern(/^[a-zA-Z\s'-]+$/)]],
+      specialization: [null,[Validators.required]], 
+      contact: [null,[Validators.required, Validators.minLength(10), Validators.maxLength(15), Validators.pattern(/^[0-9]+$/)]], 
+      status:[true,[Validators.required]]
+    })
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if(this.action && this.action != "ADD"){
+      this.setDoctorForm();
+    }
+  }
+
+  ngOnInit() {
+
+  }
+
+  setDoctorForm(){
+    this.doctorForm.get('name')?.setValue(this.data?.name);
+    this.doctorForm.get('specialization')?.setValue(this.data?.specialization);
+    this.doctorForm.get('contact')?.setValue(this.data?.contact);
+    this.doctorForm.get('status')?.setValue(this.data?.status);
+  }
+
+  saveDoctorDetails(){
+    let closeButton: any = document.getElementById("closeDoctorFormModal")
+    closeButton.click();
+    this.closeModal(true);
+  }
+
+  closeModal(formSave: boolean){
+    this.doctorFormSubmit.emit(formSave);
+    this.doctorForm.reset();
+    this.doctorForm.get('status')?.setValue(true);
+  }
 }
--- src/app/doctor-list/doctor-list.component.ts
+++ src/app/doctor-list/doctor-list.component.ts
@@ -1,26 +1,37 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { DoctorFormComponent } from "./doctor-form/doctor-form.component";
 
 @Component({
   selector: 'app-doctor-list',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule, MatPaginatorModule, DoctorFormComponent],
   templateUrl: './doctor-list.component.html',
   styleUrl: './doctor-list.component.css'
 })
-export class DoctorListComponent {
+export class DoctorListComponent implements OnInit {
   doctorListData = [
-    {name:'Vasanth',specialization:'Cardiologist',contact:'9098712345',status:true},
-    {name:'Kumar',specialization:'Cardiac Electrophysiologist',contact:'9097812345',status:false},
-    {name:'Mari',specialization:'Neurologist',contact:'9097612345',status:true},
-    {name:'Manoj',specialization:'Neurosurgeon',contact:'9097512345',status:false},
-    {name:'Dhanu',specialization:'Nuclear Medicine Specialist',contact:'9097412345',status:true},
-  ]
+    { name: 'Vasanth', specialization: 'Cardiologist', contact: '9098712345', status: true },
+    { name: 'Kumar', specialization: 'Cardiac Electrophysiologist', contact: '9097812345', status: false },
+    { name: 'Mari', specialization: 'Neurologist', contact: '9097612345', status: true },
+    { name: 'Manoj', specialization: 'Neurosurgeon', contact: '9097512345', status: false },
+    { name: 'Dhanu', specialization: 'Nuclear Medicine Specialist', contact: '9097412345', status: true },
+  ];
+  specializationList = ['Cardiologist', 'Cardiac Electrophysiologist', 'Neurologist', 'Neurosurgeon', 'Nuclear Medicine Specialist'];
+  action: string = "";
+  data:any;
 
-  constructor(){
+  constructor() {
 
   }
 
-  ngOnInit(){
+  ngOnInit() {
 
+  }
+
+  doctorFormSubmit(event: boolean) {
+    this.action = "";
+    this.data = null;
   }
 }
